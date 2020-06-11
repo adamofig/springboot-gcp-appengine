403a34341532d2169dd6cefb02dde518101ae597
Adamo Figueroa <adamo.figueroa@gnp.com.mx>
2020-06-11 01:51:29 -0500

    primer commit

---
+++ src/main/java/com/example/demo/controllers/HolaController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HolaController {
+    @GetMapping()
+    public String inicio(){
+        return "Hola esta funcionando";
+    }
+}
